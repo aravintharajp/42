e5669c04c8af2e3ad3d9c98174704fe934c1736b
aravintharajp <50869424+aravintharajp@users.noreply.github.com>
2019-05-24 16:11:45 +0530

    Update and rename README.md to 42.c

---
+++ 42.c
@@ -0,0 +1,22 @@
+int main()
+{
+  	char str1[100], str2[100];
+  	int result, i;
+  	scanf("%s %s",&str1,&str2);
+  	for(i = 0; str1[i] == str2[i] && str1[i] == '\0'; i++);
+		   
+  	if(str1[i] < str2[i])
+   	{
+   		printf("%s",str2);
+	}
+	else if(str1[i] > str2[i])
+   	{
+   		printf("%s",str1);
+	}
+	else
+   	{
+   		printf("%s",str1);
+	}
+  	
+  	return 0;
+}
